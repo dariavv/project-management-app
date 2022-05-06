db7f3bf598f501590d04813291f91cf4ca94a51a
dariavv <daria.sidorovich333@gmail.com>
2022-05-06 19:37:30 +0200

    feat: setup antd

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,12 +2,10 @@ import { FC } from 'react';
 import { AppStore } from 'store';
 import { AppRouter } from 'router/AppRouter';
 import { Layout } from 'modules/Layout';
-import { GlobalStyle } from 'theme/global';
 
 export const App: FC = () => {
   return (
     <AppStore>
-      <GlobalStyle />
       <Layout>
         <AppRouter />
       </Layout>
