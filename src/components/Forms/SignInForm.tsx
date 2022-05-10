f44cc20a392bbd1626d0025a00171686487474ec
GBaykov <gleb.baykov99@gmail.com>
2022-05-11 01:16:29 +0300

    PMA-12 feat: implement styled-components, antd in the SignInForm

--- src/components/Forms/SignInForm.tsx
+++ src/components/Forms/SignInForm.tsx
@@ -1,57 +1,101 @@
+import React from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import styled from 'styled-components';
-import { Form, Input, Button } from 'antd';
+import { Form, Input } from 'antd';
+import { Button } from 'components/Button';
+
+export interface ISignIn {
+  login: string;
+  password: string;
+}
+
+const StyledForm = styled.section`
+  padding: 5%;
+  width: 80%;
+  margin: 2% auto;
+  border: 1px solid black;
+`;
+const StyledErrorMessage = styled.p`
+  color: red;
+`;
+
 const SignInForm = () => {
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<ISignIn>({ mode: 'onSubmit' });
+
+  const onSubmit: SubmitHandler<ISignIn> = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Login"
-        name="login"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Login!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
+    <StyledForm>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
         wrapperCol={{
-          offset: 8,
           span: 16,
         }}
+        initialValues={{
+          remember: true,
+        }}
+        autoComplete="off"
+        onFinish={handleSubmit(onSubmit)}
       >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item label="Login">
+          <Controller
+            control={control}
+            name="login"
+            render={({ field }) => (
+              <Input
+                {...field}
+                onChange={(e) => {
+                  field.onChange(e);
+                }}
+              />
+            )}
+            rules={{ required: true }}
+          />
+          <StyledErrorMessage className="error-message">
+            {errors.login && '*Please input your Login!'}
+          </StyledErrorMessage>
+        </Form.Item>
+        <Form.Item label="Password">
+          <Controller
+            control={control}
+            name="password"
+            render={({ field }) => (
+              <Input
+                {...field}
+                onChange={(e) => {
+                  field.onChange(e);
+                }}
+              />
+            )}
+            rules={{ required: true }}
+          />
+          <StyledErrorMessage className="error-message">
+            {errors.password && '*Please input your password!'}
+          </StyledErrorMessage>
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </StyledForm>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,12 @@ import { AppStore } from 'store';
 import 'theme/normalize.css';
 import 'theme/common.css';
 import 'locales/i18n';
-import SignInForm from 'components/Forms/SignInForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AppStore>
-        <SignInForm />
         <App />
       </AppStore>
     </BrowserRouter>
