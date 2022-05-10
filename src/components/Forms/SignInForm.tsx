9ac3afbebeefec2f7b131fa194e2533cd83f117c
GBaykov <gleb.baykov99@gmail.com>
2022-05-10 23:01:38 +0300

    feat: add basic SignIn markup

---
+++ src/components/Forms/SignInForm.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { Form, Input, Button } from 'antd';
+const SignInForm = () => {
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Login"
+        name="login"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Login!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default SignInForm;
--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,14 @@ import { AppStore } from 'store';
 import 'theme/normalize.css';
 import 'theme/common.css';
 import 'locales/i18n';
+import SignInForm from 'components/Forms/SignInForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AppStore>
+        <SignInForm />
         <App />
       </AppStore>
     </BrowserRouter>
