1912af30046aae525a3fedb3dfef0762b7e1299f
GBaykov <gleb.baykov99@gmail.com>
2022-05-15 22:59:13 +0300

    PMA-31 feat: add basic 404 markup

---
+++ src/modules/NotFound/styled.ts
@@ -0,0 +1,42 @@
+import { GREY, PRIMARY } from 'constants/colors';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const ContainerNotFound = styled.section`
+  width: 100%;
+  text-align: center;
+`;
+export const NotFoundTop = styled.section`
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+export const NotFoundTopInner = styled.section`
+  background-image: url(/404-bg.png);
+  background-position: center;
+  background-size: auto 60%;
+  background-repeat: no-repeat;
+`;
+export const NFHeading = styled.h1`
+  color: ${GREY};
+  font-family: varela round;
+  font-size: 10.5vh;
+  line-height: 10.5vh;
+  font-weight: 400;
+  text-transform: uppercase;
+`;
+export const NFText = styled.p`
+  color: ${GREY};
+  font-family: varela round;
+  font-size: 4.71vh;
+  line-height: 7.35vh;
+  font-weight: 400;
+  margin: 2vh 0 3vh;
+`;
+export const StyledLink = styled(Link)`
+  color: ${PRIMARY};
+  font-size: 3.71vh;
+  line-height: 4.35vh;
+`;
