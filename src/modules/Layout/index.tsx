05260220fc22c327a3de9485ebf2277666a3be55
Olga <klokova.plus@gmail.com>
2022-05-19 12:35:40 +0200

    fiz

--- src/modules/Layout/index.tsx
+++ src/modules/Layout/index.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from 'react';
 import { Footer, Header } from 'components';
-import { ScrollConteiner } from './styled';
+import * as Styled from './styled';
 
 type LayoutProps = {
   children: ReactNode;
@@ -8,10 +8,10 @@ type LayoutProps = {
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <ScrollConteiner>
+    <Styled.ScrollConteiner>
       <Header />
       {children}
       <Footer />
-    </ScrollConteiner>
+    </Styled.ScrollConteiner>
   );
 };
