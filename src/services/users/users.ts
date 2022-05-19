0afc4a66d73fc0a37425986e477a2890f83c734f
GBaykov <gleb.baykov99@gmail.com>
2022-05-19 22:49:43 +0300

    PMA-33 feat: add service of Users requests

---
+++ src/services/users/users.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { API_URL } from 'constants/index';
+import { User } from 'types';
+import { authHeader } from 'services/auth/authHeader';
+
+interface UserResponse {
+  response: User;
+}
+
+interface UsersResponse {
+  response: User[];
+}
+interface UpdateUserParams {
+  id: string;
+  name: string;
+  login: string;
+  password: string;
+}
+interface DeleteUserResponse {
+  response: string;
+}
+
+const getAllUsers = async () => {
+  const response = await axios.get<UsersResponse>(`${API_URL}users`, {
+    headers: authHeader(),
+  });
+  return response.data;
+};
+
+const getUser = async (userId: User['id']) => {
+  const response = await axios.get<UserResponse>(`${API_URL}users/${userId}`, {
+    headers: authHeader(),
+  });
+  return response.data;
+};
+
+const updateUser = async ({ id, name, login, password }: UpdateUserParams) => {
+  const response = await axios.put<UpdateUserParams>(
+    `${API_URL}users/${id}`,
+    { name, login, password },
+    {
+      headers: authHeader(),
+    },
+  );
+  return response.data;
+};
+
+const deleteUser = async (userId: User['id']) => {
+  const response = await axios.delete<DeleteUserResponse>(`${API_URL}boards/${userId}`, {
+    headers: authHeader(),
+  });
+  return { data: response.data, userId };
+};
